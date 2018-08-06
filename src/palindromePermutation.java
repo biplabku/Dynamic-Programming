83361159bd427379ffd41a175ddc26263b74ca3d
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-08-06 17:42:38 -0500

    Feat - New Feature. Adding palindrome permutation code

--- src/palindromePermutation.java
+++ src/palindromePermutation.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
 
 public class palindromePermutation {
+
+
+    public boolean getPalindromePermutation(String str) {
+        HashSet<Character> unpairedChar = new HashSet<>();
+        for(char c : str.toCharArray()) {
+            if(unpairedChar.contains(c)) {
+                unpairedChar.remove(c);
+            }else {
+                unpairedChar.add(c);
+            }
+        }
+        return unpairedChar.size() <= 1;
+    }
+
+    public static void main(String[] args) {
+        String str = "hello";
+        palindromePermutation ss = new palindromePermutation();
+        System.out.println(ss.getPalindromePermutation(str));
+    }
 }
